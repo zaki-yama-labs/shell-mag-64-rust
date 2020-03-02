1c6a4aa03f58a32768ac461e30c1733128779e98
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-03-02 23:22:14 +0900

    [p21] コマンドライン引数を必須にする

--- trip-analyzer/src/main.rs
+++ trip-analyzer/src/main.rs
@@ -9,10 +9,11 @@ fn main() {
         .arg(Arg::with_name("INFILE")
             .help("Sets the input CSV file")
             .index(1) // 最初の引数
+            .required(true)
         )
         // get_matches() メソッドを呼ぶとユーザーが与えた
         // コマンドライン引数がパースされる
         .get_matches();
-    // INFILE の文字列を表示。"{:?}"はデバッグ用文字列を表示
-    println!("INFILE: {:?}", arg_matches.value_of("INFILE"));
+    let infile = arg_matches.value_of("INFILE").unwrap();
+    println!("INFILE: {}", infile);
 }
